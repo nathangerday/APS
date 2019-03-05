6496fc9bd9b874b5f94f01b2eda2a0710e907ada
Nathan GERDAY <nat.gerday@gmail.com>
2019-03-05 09:04:12 +0100

    Add prim + - / *

--- Ast/AstPrim.java
+++ Ast/AstPrim.java
@@ -23,6 +23,31 @@ public class AstPrim implements IASTExpr {
 
 
     public Value eval(Environment env){
+        Integer val1 = a1.eval(env).getN();
+        Integer val2 = null;
+        if(a2 != null){
+            val2 = a2.eval(env).getN();
+        }
+        switch(op){
+            case ADD:
+                return new Value(val1+val2);
+            case SUB:
+                return new Value(val1-val2);
+            case MUL:
+                return new Value(val1*val2);
+            case DIV:
+                return new Value(val1/val2);
+            case NOT:
+                return new Value(1);
+            case AND:
+                return new Value(1);
+            case OR:
+                return new Value(1);
+            case EQ:
+                return new Value(1);
+            case LT:
+                return new Value(1);
+        }
         return null;
     }
     
